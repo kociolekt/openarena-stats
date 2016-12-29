f18e48c2e4215feeea0efcf15ab3302c30ae37ab
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-12-29 13:54:54 +0100

    przyklady wynikow i cors

--- server.js
+++ server.js
@@ -105,6 +105,15 @@ router.get('/games', function(req, res) {
   res.send(JSON.stringify(jc.decycle(jt(games, 3))));
 });
 
+var allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
